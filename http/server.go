bfffc2bd971a718265d648b7f372bf9a005cc412
Matheus Silva <matheus.silva@in8.com.br>
2024-03-26 17:28:33 -0300

    add more things to http

--- http/server.go
+++ http/server.go
@@ -1,20 +1,48 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"net/url"
 )
 
 type uai int
 
 func (m uai) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprint(w, "aaahhahaha")
+	err := req.ParseForm()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	data := struct {
+		Method        string
+		URL           *url.URL
+		Submissions   map[string][]string
+		Header        http.Header
+		Host          string
+		ContentLength int64
+	}{
+		req.Method,
+		req.URL,
+		req.Form,
+		req.Header,
+		req.Host,
+		req.ContentLength,
+	}
+
+	w.Header().Set("Mathesukkj-Header", "header customizado pai receba")
+
+	tpl.ExecuteTemplate(w, "index.gohtml", data)
+}
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("index.gohtml"))
 }
 
 func main() {
 	var handler uai
-	err := http.ListenAndServe(":5051", handler)
-	if err != nil {
-		fmt.Println("errorororororor", err)
-	}
+	http.ListenAndServe(":5051", handler)
 }
