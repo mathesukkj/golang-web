1f05213744f50a578aa0c9bd615fdd1f11214876
mathesukkj <kemuel.g7363@gmail.com>
2024-03-27 22:29:27 -0300

    start using handleFunc with DefaultServeMux

--- http-servemux/server.go
+++ http-servemux/server.go
@@ -21,9 +21,18 @@ func main() {
 	var dog handlerDog
 	var cat handlerCat
 
+	// using Mux
 	mux := http.NewServeMux()
-	mux.Handle("/dog", dog)
 	mux.Handle("/cat", cat)
 
-	http.ListenAndServe(":4080", mux)
+	// using DefaultServeMux and Handle
+	http.Handle("/dog", dog)
+
+	// using DefaultServeMux and HandleFunc
+	http.HandleFunc("/wolf", func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "auuuuuuuuuuuu")
+	})
+
+	//http.ListenAndServe(":4080", mux)
+	http.ListenAndServe(":4080", nil)
 }
