92b4a609415b5a2952f50b1803a33148b48150e5
mathesukkj <kemuel.g7363@gmail.com>
2024-03-29 16:36:00 -0300

    serve files with http.ServeFile

--- http-servefiles/server.go
+++ http-servefiles/server.go
@@ -3,7 +3,6 @@ package main
 import (
 	"io"
 	"net/http"
-	"os"
 )
 
 func main() {
@@ -19,20 +18,21 @@ func index(w http.ResponseWriter, r *http.Request) {
 }
 
 func serveFile(w http.ResponseWriter, r *http.Request) {
-	f, err := os.Open("images.jpeg")
-	if err != nil {
-		http.Error(w, "file not found", 404)
-		return
-	}
-	defer f.Close()
+	// f, err := os.Open("images.jpeg")
+	// if err != nil {
+	// 	http.Error(w, "file not found", 404)
+	// 	return
+	// }
+	// defer f.Close()
+	//
+	// fi, err := f.Stat()
+	// if err != nil {
+	// 	http.Error(w, "file not found", 404)
+	// 	return
+	// }
 
-	fi, err := f.Stat()
-	if err != nil {
-		http.Error(w, "file not found", 404)
-		return
-	}
-
-	http.ServeContent(w, r, f.Name(), fi.ModTime(), f)
+	http.ServeFile(w, r, "images.jpeg")
+	//	http.ServeContent(w, r, f.Name(), fi.ModTime(), f)
 	// io.Copy(w, f)
 
 }
