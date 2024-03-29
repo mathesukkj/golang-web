5f4a6a6759861afe7c7d4e3eaa7ed1798d596733
mathesukkj <kemuel.g7363@gmail.com>
2024-03-29 16:46:47 -0300

    fileserver with StripPrefix

--- http-fileserver/server.go
+++ http-fileserver/server.go
@@ -7,12 +7,16 @@ import (
 
 func main() {
 
-	http.Handle("/", http.FileServer(http.Dir(".")))
+	//	http.Handle("/", http.FileServer(http.Dir(".")))
+	http.Handle(
+		"/resources/",
+		http.StripPrefix("/resources", http.FileServer(http.Dir("./resources"))),
+	)
 	http.HandleFunc("/index", index)
 	http.ListenAndServe(":4050", nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	io.WriteString(w, "<img src='images.jpeg' />")
+	io.WriteString(w, "<img src='/resources/images.jpeg' />")
 }
