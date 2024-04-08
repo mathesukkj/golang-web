34144a497479d916c19f42a08ad0250e85861e0a
mathesukkj <kemuel.g7363@gmail.com>
2024-04-07 23:39:25 -0300

    add mysql

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module mathesukkj/goudemyweb
 go 1.21.0
 
 require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	github.com/julienschmidt/httprouter v1.3.0 // indirect
 	github.com/satori/go.uuid v1.2.0 // indirect
 	golang.org/x/crypto v0.21.0 // indirect
---
+++ http-sql/server.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+var err error
+
+func main() {
+	db, err = sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("connected!!")
+}
